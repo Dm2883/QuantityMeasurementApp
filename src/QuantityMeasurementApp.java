6e9340300eba67049498b4e497822db55980c10c
Dm2883 <devarajmurugan45@gmail.com>
2026-04-22 09:54:17 +0530

    Initial commit - UC1 setup

---
+++ src/QuantityMeasurementApp.java
@@ -0,0 +1,44 @@
+package com.apps.quantitymeasurement;
+
+public class QuantityMeasurementApp {
+
+    // Inner class to represent a Feet measurement
+    static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            // Reflexive: same reference
+            if (this == obj) return true;
+
+            // Null check
+            if (obj == null) return false;
+
+            // Type check
+            if (this.getClass() != obj.getClass()) return false;
+
+            // Cast and compare
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=== Quantity Measurement App ===");
+        System.out.println("--- UC1: Feet Measurement Equality ---\n");
+
+        Feet feet1 = new Feet(1.0);
+        Feet feet2 = new Feet(1.0);
+        Feet feet3 = new Feet(2.0);
+
+        System.out.println("1.0 ft equals 1.0 ft: " + feet1.equals(feet2));
+        System.out.println("1.0 ft equals 2.0 ft: " + feet1.equals(feet3));
+        System.out.println("1.0 ft equals null: " + feet1.equals(null));
+        System.out.println("1.0 ft equals \"abc\": " + feet1.equals("abc"));
+        System.out.println("1.0 ft equals itself: " + feet1.equals(feet1));
+    }
+}
